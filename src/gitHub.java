b9c675bb18a72e3524bd60d3fa174c473a73a2a2
TREUSCHJE21 <TreuschJE21@gcc.edu>
2022-02-23 09:52:39 -0500

    go

--- src/gitHub.java
+++ src/gitHub.java
@@ -2,5 +2,7 @@ public class gitHub {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Your mom doesn't have any kids");
+
+        System.out.println("tortured squirrel");
     }
 }
