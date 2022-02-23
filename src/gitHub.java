a5eefe5b393ce95188e78980474cb3cd74b9f38a
Jsouthern777 <southernjj21@gcc.edu>
2022-02-23 09:42:08 -0500

    Initial Commit

--- src/gitHub.java
+++ src/gitHub.java
@@ -1,5 +1,6 @@
 public class gitHub {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Your mom doesn't have any kids");
     }
 }
